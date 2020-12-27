d9a842427e797ee24e733f9f14ea3c2f549c2746
salty-walty <waltonsj3@gmail.com>
2020-12-27 09:38:07 -0500

    start of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Calculator from './Calculator'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Gimme Phi!</h1>
+      <p>
+        Drop a number in the field below and see it's corresponding value when multiplied by Phi.
+      </p>
+      <Calculator />
     </div>
   );
 }
---
+++ src/Calculator.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './Calculator.css'
+
+function Calculator() {
+  return (
+    <div className="calc">
+      <h1>This is where the Calculator will go</h1>
+    </div>
+  );
+}
+
+export default Calculator;
